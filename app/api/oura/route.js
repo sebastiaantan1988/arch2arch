2307ff8a4f2cff43d73b2b15754e565323d516f3
Sebastiaan Tan <sebastiaantan@macbook-air-64.lan>
2026-02-18 10:41:02 +0100

    Fix Oura OAuth scopes

--- app/api/oura/route.js
+++ app/api/oura/route.js
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const clientId = process.env.OURA_CLIENT_ID;
   const redirectUri = process.env.OURA_REDIRECT_URI;
-  const scopes = 'personal daily heartrate workout session spo2';
+  const scopes = 'email personal daily heartrate tag workout session spo2 ring_configuration stress heart_health';
 
-  const url = `https://cloud.ouraring.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&state=arch2arch`;
+  const url = `https://cloud.ouraring.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scopes)}`;
 
   return NextResponse.redirect(url);
 }
